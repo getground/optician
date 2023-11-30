c4724706eb7fc11ded6aba042788ca26be6ed1de
Mariana Fernandes <mariana.fernandes@getground.co.uk>
2023-11-30 12:04:56 +0000

    change logger

--- src/optician/logger.py
+++ src/optician/logger.py
@@ -2,27 +2,31 @@ import logging
 
 
 class Logger:
-    def __init__(self, log_to_console=True, log_to_file=True, log_file="optician.log"):
+    def __init__(self, log_file="optician.log"):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
 
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        self.log_file = log_file
+        self.formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-        if log_to_file:
-            file_handler = logging.FileHandler(log_file)
-            file_handler.setLevel(logging.DEBUG)
-            file_handler.setFormatter(formatter)
-            self.logger.addHandler(file_handler)
-
-        if log_to_console:
+    def get_logger(self, log_to_console=True, log_to_file=True):
+        # Check if console handler already exists
+        if log_to_console and not any(
+            isinstance(handler, logging.StreamHandler)
+            for handler in self.logger.handlers
+        ):
             console_handler = logging.StreamHandler()
             console_handler.setLevel(logging.INFO)
-            console_handler.setFormatter(formatter)
+            console_handler.setFormatter(self.formatter)
             self.logger.addHandler(console_handler)
 
-    def get_logger(self):
-        l = logging.getLogger(__name__)
-        if l.hasHandlers():
-            return l
-        else:
-            return self.logger
+        # Check if file handler already exists
+        if log_to_file and not any(
+            isinstance(handler, logging.FileHandler) for handler in self.logger.handlers
+        ):
+            file_handler = logging.FileHandler(self.log_file)
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(self.formatter)
+            self.logger.addHandler(file_handler)
+
+        return self.logger
